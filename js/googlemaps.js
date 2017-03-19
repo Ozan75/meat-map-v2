578080e407382e07df2ceb4d981da1bed2f34f7f
Ozan75 <ozan@ozan75.de>
2017-03-19 22:59:07 +0100

    Added JSON Marker

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,50 +1,105 @@
 function initMap() {
 
-var map = new google.maps.Map(document.getElementById('meat-map'), {
-  center: lastPos,
-  zoom: 15,
-  disableDefaultUI: true,
-  styles: mapStyle // Styles a map in night mode.
-});
+    var map = new google.maps.Map(document.getElementById('meat-map'), {
+        center: lastPos,
+        zoom: 15,
+        disableDefaultUI: true,
+        styles: mapStyle // Styles a map in night mode.
+    });
+
+    // Creating the JSON data
+    var json = [{
+            "title": "To Beef or not to Beef",
+            "lat": 52.486645,
+            "lng": 13.356394,
+            "description": "<strong>To Beef or not to Beef</strong>Lorem ipsum dolor sit amet"
+        },
+        {
+            "title": "Burgers Berlin",
+            "lat": 52.494450,
+            "lng": 13.353629,
+            "description": "<strong>Burgers Berlin</strong>Lorem ipsum dolor sit amet"
+        },
+        {
+            "title": "Chicago Williams BBQ",
+            "lat": 52.527111,
+            "lng": 13.385833,
+            "description": "<strong>Chicago Williams BBQ</strong>"
+        }
+    ]
+
+    // Creating a global infoWindow object that will be reused by all markers
+    var infoWindow = new google.maps.InfoWindow();
+
+    // Looping through the JSON data
+    for (var i = 0, length = json.length; i < length; i++) {
+        var data = json[i],
+            latLng = new google.maps.LatLng(data.lat, data.lng);
+
+        // Creating a marker and putting it on the map
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: data.title
+        });
+
+        // Creating a closure to retain the correct data, notice how I pass the current data in the loop into the closure (marker, data)
+        (function(marker, data) {
+
+            // Attaching a click event to the current marker
+            google.maps.event.addListener(marker, "click", function(e) {
+                infoWindow.setContent(data.description);
+                infoWindow.open(map, marker);
+            });
 
 
-var icon = {
-    url: "https://www.meat-map.com/svg/geolocation.svg",
-    fillOpacity: .9,
-    anchor: new google.maps.Point(3,18),
-    strokeWeight: 0,
-    scaledSize: new google.maps.Size(30, 36),
-};
+        })(marker, data);
 
-var marker = new google.maps.Marker({position: lastPos, map: map, icon: icon}); // Set special Marker to last position.
+    }
 
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
+    // Define Marker
+    var blueMarker = {
+        url: "https://www.meat-map.com/svg/geolocation.svg",
+        fillOpacity: .9,
+        anchor: new google.maps.Point(3, 18),
+        strokeWeight: 0,
+        scaledSize: new google.maps.Size(30, 36),
     };
 
-  // Saving current geo location to Local Storage as a STRING
-  localStorage.setItem('currentPos', JSON.stringify(pos));
-  var pos = JSON.parse(localStorage.getItem('currentPos'));
+    // Set special Marker to last position.
+    var geoPosition = new google.maps.Marker({
+        position: lastPos,
+        map: map,
+        icon: blueMarker
+    });
 
-  // Centering Map and repositionig Marker
-  map.setCenter(pos);
-  marker.setPosition(pos);
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
 
-  }, function() {
-    // Information send to HandleLocationError Function - Browser supports Geolocation
-    handleLocationError(true, marker, map, map.getCenter());
-  });
-} else {
-  // Information send to HandleLocationError Function - Browser doesn't support Geolocation
-  handleLocationError(false, marker, map, lastPos); //Should be set to a default LatLang
-};
-}; setTimeout(initMap, 20); // Timeout length & END OF MAP INIT
+            // Saving current geo location to Local Storage as a STRING
+            localStorage.setItem('currentPos', JSON.stringify(pos));
+            var pos = JSON.parse(localStorage.getItem('currentPos'));
 
-function handleLocationError(browserHasGeolocation, marker, map, lastPos) {
-marker.setPosition(lastPos);
-map.setCenter(lastPos);
+            // Centering Map and repositionig Marker
+            map.setCenter(pos);
+            geoPosition.setPosition(pos);
+
+        }, function() {
+            // Information send to HandleLocationError Function - Browser supports Geolocation
+            handleLocationError(true, geoPosition, map, map.getCenter());
+        });
+    } else {
+        // Information send to HandleLocationError Function - Browser doesn't support Geolocation
+        handleLocationError(false, marker, map, lastPos); //Should be set to a default LatLang
+    };
+}; // Timeout length & END OF MAP INIT
+
+function handleLocationError(browserHasGeolocation, geoPosition, map, lastPos) {
+    geoPosition.setPosition(lastPos);
+    map.setCenter(lastPos);
 }
