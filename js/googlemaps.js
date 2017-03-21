3179854075e66e2de2091f2cf725c36fbe0c460f
Ozan75 <ozan@ozan75.de>
2017-03-21 21:51:16 +0100

    header transparency, center on marker, new markers

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -15,34 +15,46 @@ function initMap() {
         var data = jsonMarker[i],
             latLng = new google.maps.LatLng(data.lat, data.lng);
 
-    // Creating a marker and putting it on the map
+        // Creating red marker
+        var redMarker = {
+            url: "https://www.meat-map.com/svg/marker-red.svg",
+            fillOpacity: .9,
+            anchor: new google.maps.Point(5, 50),
+            strokeWeight: 0,
+            scaledSize: new google.maps.Size(43, 49),
+        };
+        // Putting Marker on the map
         var marker = new google.maps.Marker({
             position: latLng,
             map: map,
+            icon: redMarker,
             title: data.title
         });
 
-    // Creating a closure to retain the correct data, notice how I pass the current data in the loop into the closure (marker, data)
+        // Creating a closure to retain the correct data, notice how I pass the current data in the loop into the closure (marker, data)
         (function(marker, data) {
 
-    // Attaching a click event to the current marker
+            // Attaching a click event to the current marker
             google.maps.event.addListener(marker, "click", function(e) {
-                infoWindow.setContent(data.description);
-                infoWindow.open(map, marker);
+                map.setCenter(marker.getPosition());
+                var d = document.getElementById("infobox");
+                d.className += "box_slide_in";
+                //infoWindow.setContent(data.description);
+                //infoWindow.open(map, marker);
             });
         })(marker, data);
     }
 
-    // Define Marker
+    // Creating blue marker
     var blueMarker = {
         url: "https://www.meat-map.com/svg/geolocation.svg",
         fillOpacity: .9,
-        anchor: new google.maps.Point(3, 18),
+        anchor: new google.maps.Point(24, 24),
         strokeWeight: 0,
-        scaledSize: new google.maps.Size(30, 36),
+        scaledSize: new google.maps.Size(48, 48),
     };
 
-    // Set special Marker to last position.
+    // Putting Marker on the map
     var geoPosition = new google.maps.Marker({
         position: lastPos,
         map: map,
@@ -57,22 +69,33 @@ function initMap() {
                 lng: position.coords.longitude
             };
 
-    // Saving current geo location to Local Storage as a STRING
+            // Saving current geo location to Local Storage as a STRING
             localStorage.setItem('currentPos', JSON.stringify(pos));
             var pos = JSON.parse(localStorage.getItem('currentPos'));
 
-    // Centering Map and repositionig Marker
+            // Centering Map and repositionig Marker
             map.setCenter(pos);
             geoPosition.setPosition(pos);
 
         }, function() {
-    // Information send to HandleLocationError Function - Browser supports Geolocation
+            // Information send to HandleLocationError Function - Browser supports Geolocation
             handleLocationError(true, geoPosition, map, map.getCenter());
         });
     } else {
-    // Information send to HandleLocationError Function - Browser doesn't support Geolocation
+        // Information send to HandleLocationError Function - Browser doesn't support Geolocation
         handleLocationError(false, marker, map, lastPos); //Should be set to a default LatLang
     };
+
+    // Adding transparency toe overlying header while drgging map
+    google.maps.event.addListener(map, 'drag', function() {
+        var d = document.getElementById("header");
+        d.className += " map_drag";
+    });
+    google.maps.event.addListener(map, 'dragend', function() {
+        var d = document.getElementById("header");
+        d.className -= " map_drag";
+    });
+
 }; //END OF MAP INIT //////////////////////////////////////////
 
 function handleLocationError(browserHasGeolocation, geoPosition, map, lastPos) {
